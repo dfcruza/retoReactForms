466756972cfbe60334da6e569e81bf6bdc31ceb1
dfcruza <60239198+dfcruza@users.noreply.github.com>
2023-09-22 22:49:46 -0500

    update

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   const handlePasswordChange = ((e) =>  {
     setFormValues({...formValues, password: e.target.value})
-    if(e.target.value.length < 9&& e.target.value.match(/[a-z]/i) && e.target.value.match(/[0-9]/i)){
+    if(e.target.value.length < 9 && e.target.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/) && e.target.value.match(/[0-9]/i)){
       setErrorMessage({...errorMessage, password: false})
       alert("Password is not valid")
     }
@@ -56,37 +56,33 @@ function App() {
       alert("Password is not valid")}
     alert(JSON.stringify(formValues))
   });
-
- return (
-   <div>
-     <h1>¡Ejemplo de formularios!</h1>
-    
-     <Form>
-     <Form.Group className="mb-6" controlId="formBasicEmail">
-       <Form.Label>Email address</Form.Label>
-       <Form.Control type="email" placeholder="Enter email"/>
-       <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-     </Form.Group>
-
-     <Form.Group className="mb-3" controlId="formBasicPassword">
-       <Form.Label>Password</Form.Label>
-       <Form.Control type="password" placeholder="Password" />
-       <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>
-     </Form.Group>
-     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-       <Form.Label>Favorite Class</Form.Label>
-       <Form.Select>
-         <option value="1">ISIS3710</option>
-         <option value="2">Programación con tecnologias web</option>
-       </Form.Select>
-     </Form.Group>
-     <Button variant="primary" onClick={clickSubmit}>
-       Submit
-     </Button>
-   </Form>
-   </div>
- );
+return (
+  <div>
+    <h1>Ejemplo de formularios!</h1>
+     
+    <Form>
+    <Form.Group className="mb-6" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>        <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} value={formValues.email}/>
+        { !validationStates.emailState && <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>}
+      </Form.Group>
+ 
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} value={formValues.password} />
+        { !validationStates.passwordState && <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Label>Favorite Class</Form.Label>
+        <Form.Select onChange={handleSelectChange}>
+          <option value="1">ISIS3710</option>
+          <option value="2">Programación con tecnologias web</option>
+        </Form.Select>
+      </Form.Group>
+      <Button variant="primary">
+        Submit
+      </Button>
+  </Form>
+  </div>
+);
 }
-
-
 export default App;
